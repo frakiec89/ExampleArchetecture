aeed5c288779fc4d9c02f30376cf001b312e8701
Ahtyamov <Ahtyamov@WIN-SFUQ8BAFB06>
2023-03-14 09:45:55 +0400

    кукукук

---
+++ ConsoleApp54/MyConsole.cs
@@ -0,0 +1,45 @@
+﻿using ConsoleApp54.Core;
+using System;
+using System.Collections.Generic;
+namespace ConsoleApp54
+{
+    internal class MyConsole 
+    {
+        public IStudetnService Service { get; }
+        public MyConsole(IStudetnService service) // проброс  зависимости
+        {
+            Service = service;
+        }
+
+        public void  Start () 
+        {
+            foreach (Student st in Service.Students)
+
+            {
+                Console.WriteLine(st.Name);
+            }
+
+        }
+    }
+
+    public class   Bild
+    {
+
+       public static IStudetnService GetService(TypeContent type = TypeContent.Mock)
+        {
+            switch (type)
+            {
+                case TypeContent.Mock:
+                    return new ConsoleApp54.DB_Mock.StudentService();
+                case TypeContent.Class1:
+                    return new ConsoleApp54.Content2.StudentService();
+            }
+            return new ConsoleApp54.Content2.StudentService();
+        }
+    }
+
+    public enum TypeContent
+    {
+        Mock, Class1
+    }
+}
--- ConsoleApp54/Program.cs
+++ ConsoleApp54/Program.cs
@@ -1,34 +1,7 @@
-﻿using ConsoleApp54.Core;
-using ConsoleApp54.DB_Mock;
-using ConsoleApp54.Content2;
+﻿
+using ConsoleApp54;
 
-IStudetnService studetnService
-    = GetService(TypeContent.Class1); // todo??? зависимость
+MyConsole console = new MyConsole(Bild.GetService
+    (TypeContent.Mock));
 
-
-foreach (Student st in 
-    
-    studetnService.Students)
-
-{
-    Console.WriteLine(st.Name);
-}
-
-IStudetnService GetService(TypeContent type = TypeContent.Mock)
-{
-    switch (type)
-    {
-        case TypeContent.Mock:
-           return new ConsoleApp54.DB_Mock.StudentService();
-        case TypeContent.Class1:
-            return new ConsoleApp54.Content2.StudentService();
-        default:
-            break;
-    }
-    return new ConsoleApp54.Content2.StudentService();
-}
-
-public enum TypeContent
-{
-    Mock , Class1 
-}
+console.Start();
