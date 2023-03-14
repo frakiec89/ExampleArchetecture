f9061efcc39cd1dd103dfa23691edd14032dc4d9
Ahtyamov <Ahtyamov@WIN-SFUQ8BAFB06>
2023-03-14 09:09:06 +0400

    dsdsds

--- ConsoleApp54.Content2/Class1.cs
+++ ConsoleApp54.Content2/Class1.cs
@@ -4,13 +4,20 @@ namespace ConsoleApp54.Content2
 {
     internal  class Class1
     {
-        static string Name = "Ivan";
-
-
+       internal static string Name = "Ivan";
     }
 
     public class StudentService : IStudetnService
     {
-
+        public List<Student> Students 
+        { get
+            {
+                return new List<Student>() 
+                { new Student { Name = Class1.Name } };
+            }
+            set
+            { 
+            } 
+        }
     }
 }
